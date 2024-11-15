8142c8e9c6eebaee93356343a4ded86520e48a71
Tanay Duddalwar <119003967+tanayduddalwar@users.noreply.github.com>
2024-11-15 12:08:53 +0530

    Update swaggerConfig.js

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -11,7 +11,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: 'http://localhost:8000', // Replace with your server URL
+      url: 'https://restaurent-and-delivery-backend.onrender.com', // Replace with your server URL
     },
   ],
 };
